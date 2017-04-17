9c6f28bd7b673af94825c984272c6dad7673bc89
michaelcw02 <michael.cw02@hotmail.com>
2017-04-17 12:53:44 -0600

    things

--- client-side/js/Offer.js
+++ client-side/js/Offer.js
@@ -1,4 +1,4 @@
-
+/*
 var Offer = function(ctryFrom, ctryTo, price, description, pathToImg) {
     Offer.prototype.ctryFrom = ctryFrom;
     Offer.prototype.ctryTo = ctryTo;
@@ -6,5 +6,12 @@ var Offer = function(ctryFrom, ctryTo, price, description, pathToImg) {
     Offer.prototype.description = description;
     Offer.prototype.pathToImg = pathToImg;
 }
-
+*/
+function Offer(ctryFrom, ctryTo, price, description, pathToImg) {
+    this.ctryFrom = ctryFrom;
+    this.ctryTo = ctryTo;
+    this.price = price;
+    this.description = description;
+    this.pathToImg = pathToImg;
+}
 
--- client-side/js/loadIndex.js
+++ client-side/js/loadIndex.js
@@ -1,19 +1,28 @@
 
 function loadPage() {
-
+    loadCarousel();
 }
 
+//CONEXION A BACKEND PARA RETRIEVE LAS OFERTAS
 function loadOfferList() {
     let array = [];
-    let o = new Offer('SJO', 'LAX', '$590', 'New Offer!', '../images/background-1.js');
-    let o = new Offer('SJO', 'MIA', '$600', 'New Offer!', '../images/background-1.js');
-    let o = new Offer('SJO', 'MEX', '$800', 'New Offer!', '../images/background-1.js');
-    let o = new Offer('SJO', 'LAX', '$590', 'New Offer!', '../images/background-1.js');
-    let o = new Offer('SJO', 'LAX', '$590', 'New Offer!', '../images/background-1.js');
+    array.push(new Offer('SJO', 'LAX', '$590', 'New Offer!', 'images/background-1.jpg'));
+    array.push(new Offer('SJO', 'MIA', '$600', 'New Offer!', 'images/background-2.jpg'));
+    array.push(new Offer('SJO', 'MEX', '$800', 'New Offer!', 'images/background-3.jpg'));
+    array.push(new Offer('SJO', 'BOS', '$800', 'New Offer!', 'images/background-4.jpg'));
+    array.push(new Offer('SJO', 'BOG', '$400', 'New Offer!', 'images/background-5.jpg'));
+    return array;
 }
 
 function loadCarousel() {
-
+    offerList = loadOfferList();
+    for(let i in offerList) {
+        $('<div class="item"><img src="'+offerList[i].pathToImg+'"><div class="carousel-caption"></div>   </div>').appendTo('.carousel-inner');
+        $('<li data-target="#advertisement-carousel" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
+    }
+    $('.item').first().addClass('active');
+    $('.carousel-indicators > li').first().addClass('active');
+    $('#advertisement-carousel').carousel();
 }
 
 
