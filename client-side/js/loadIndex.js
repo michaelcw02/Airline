a99e82f1d7e62ac6794941c0e8053f4bdb4330a2
michaelcw02 <michael.cw02@hotmail.com>
2017-04-17 23:01:15 -0600

    saving for another branch

--- client-side/js/loadIndex.js
+++ client-side/js/loadIndex.js
@@ -6,19 +6,19 @@ function loadPage() {
 //CONEXION A BACKEND PARA RETRIEVE LAS OFERTAS
 function loadOfferList() {
     let array = [];
-    array.push(new Offer('SJO', 'LAX', '$590', 'New Offer!', 'images/background-1.jpg'));
-    array.push(new Offer('SJO', 'MIA', '$600', 'New Offer!', 'images/background-2.jpg'));
-    array.push(new Offer('SJO', 'MEX', '$800', 'New Offer!', 'images/background-3.jpg'));
-    array.push(new Offer('SJO', 'BOS', '$800', 'New Offer!', 'images/background-4.jpg'));
-    array.push(new Offer('SJO', 'BOG', '$400', 'New Offer!', 'images/background-5.jpg'));
+    array.push(new Offer('SJO', 'LAX', '$590', 'LET\'S GO TO LOS ANGELES!', 'images/background-1.jpg'));
+    array.push(new Offer('SJO', 'MIA', '$600', 'WANT TO GO TO MIAMI?', 'images/background-2.jpg'));
+    array.push(new Offer('SJO', 'MEX', '$800', 'HOW ABOUT MEXICO?', 'images/background-3.jpg'));
+    array.push(new Offer('SJO', 'BOS', '$800', 'LET US TAKE YOU TO BOSTON!', 'images/background-4.jpg'));
+    array.push(new Offer('SJO', 'BOG', '$400', 'WANT TO MEET COLOMBIANS?', 'images/background-5.jpg'));
     return array;
 }
 
 function loadCarousel() {
     offerList = loadOfferList();
-    for(let i in offerList) {
-        $('<div class="item"><img src="'+offerList[i].pathToImg+'"><div class="carousel-caption"></div>   </div>').appendTo('.carousel-inner');
-        $('<li data-target="#advertisement-carousel" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
+    for (let i in offerList) {
+        $('<li data-target="#advertisement-carousel" data-slide-to="' + i + '"></li>').appendTo('.carousel-indicators')
+        $('<div class="item"><img src="' + offerList[i].pathToImg + '"><div class="carousel-caption h2">' + offerList[i].description + '</div>   </div>').appendTo('.carousel-inner');
     }
     $('.item').first().addClass('active');
     $('.carousel-indicators > li').first().addClass('active');
