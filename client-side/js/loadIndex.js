8a2ed4db390129db851d2e8f1eb5fcc58de3beb0
michaelcw02 <michael.cw02@hotmail.com>
2017-04-19 17:41:03 -0600

    merge ok

--- client-side/js/loadIndex.js
+++ client-side/js/loadIndex.js
@@ -20,7 +20,7 @@ function loadCarousel() {
     for (let i in offerList) {
         let element = '<li data-target="#advertisement-carousel" data-slide-to="' + i + '"></li>'; 
         $(element).appendTo('.carousel-indicators');
-        element = '<div class="item"><img src="' + offerList[i].pathToImg + '"><div class="carousel-caption h1">' + offerList[i].description + '</div>   </div>';
+        element = '<div class="item"><img class="img-rounded" src="' + offerList[i].pathToImg + '"><div class="carousel-caption h1">' + offerList[i].description + '</div>   </div>';
         $(element).appendTo('.carousel-inner');
     }
     $('.item').first().addClass('active');
@@ -28,5 +28,27 @@ function loadCarousel() {
     $('#advertisement-carousel').carousel();
 }
 
+function hideShow(){
+    $("#btnOneWay").click(function(){
+       $("#returning").hide();
+    });
+    $("#btnRoundTrip").click(function(){
+        $("#returning").show();
+    });
+}
+
+function increase(){
+    var valueSelected=$('select[id=flightsFormAdults]').val();
+    if(valueSelected != "6"){   
+        $('select[id=flightsFormAdults]').val(newValue);
+    }    
+}
+
+function decrease(){
+    var valueSelected=$('select[id=flightsFormAdults]').val();
+    if(valueSelected != "1"){   
+        $('select[id=flightsFormAdults]').val(newValue);
+    }    
+}
 
 $(loadPage);
