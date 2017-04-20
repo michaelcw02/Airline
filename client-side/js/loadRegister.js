7de706a18e77372208e7972be998144fad8a6d4b
michaelcw02 <michael.cw02@hotmail.com>
2017-04-20 17:09:33 -0600

    done are blanks

--- client-side/js/loadRegister.js
+++ client-side/js/loadRegister.js
@@ -1,7 +1,40 @@
 
 function loadRegister() {
     $('#birthdate').datepicker({minDate: new Date()});
+    addEventListeners();
 }
 
+function addEventListeners() {
+    $('#registrationForm').on('submit', doValidate);
+}
+
+function doValidate() {
+    if(areBlanks) {
+        //make the pop up!
+    }
+    
+}
+
+function areBlanks() {
+    let blanks = false;
+    blanks = isBlank($('#name')[0]);
+    blanks = isBlank($('#lastname')[0]);
+    blanks = isBlank($('#birthdate')[0]);
+    blanks = isBlank($('#email')[0]);
+    blanks = isBlank($('#password')[0]);
+    blanks = isBlank($('#passwordRepeat')[0]);
+    return blanks;
+}
+function isBlank(element) {    
+    element.classList.remove('invalid');
+	if (element.value.length == 0){
+		element.classList.add('invalid');
+		return true;
+	}
+}
+
+function submitRegistration() {
+
+}
 
 $(loadRegister);
