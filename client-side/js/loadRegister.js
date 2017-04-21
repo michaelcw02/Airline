4eb608bf2ff971d99956daa7e0a58098c8ef43c7
michaelcw02 <michael.cw02@hotmail.com>
2017-04-20 18:38:15 -0600

    google maps api

--- client-side/js/loadRegister.js
+++ client-side/js/loadRegister.js
@@ -1,18 +1,25 @@
 
 function loadRegister() {
-    $('#birthdate').datepicker({minDate: new Date()});
+    $('#birthdate').datepicker({ minDate: new Date() });
+    initAutocomplete();
     addEventListeners();
 }
 
 function addEventListeners() {
     $('#registrationForm').on('submit', doValidate);
+    $('#getDirection').on('click', getLocation);
+    $('#direction').on('focus', showMap);
+}
+
+function showMap(event) {
+    $('.map-container').show();
 }
 
 function doValidate() {
-    if(areBlanks) {
+    if (areBlanks) {
         //make the pop up!
     }
-    
+
 }
 
 function areBlanks() {
@@ -25,12 +32,12 @@ function areBlanks() {
     blanks = isBlank($('#passwordRepeat')[0]);
     return blanks;
 }
-function isBlank(element) {    
+function isBlank(element) {
     element.classList.remove('invalid');
-	if (element.value.length == 0){
-		element.classList.add('invalid');
-		return true;
-	}
+    if (element.value.length == 0) {
+        element.classList.add('invalid');
+        return true;
+    }
 }
 
 function submitRegistration() {
---
+++ client-side/js/map.js
@@ -0,0 +1,118 @@
+
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+function getLocation(event) {
+    let msg = document.getElementById('map');
+    $('.map-container').show();
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(setLocation, showError);
+    } else { 
+        msg.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function setLocation(position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let latlon = new google.maps.LatLng(lat, lon);
+
+    var myOptions = {
+    center:latlon,zoom:14,
+    mapTypeId:google.maps.MapTypeId.ROADMAP,
+    mapTypeControl:false,
+    navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+    }
+    
+    var map = new google.maps.Map(document.getElementById("map"), myOptions);
+    var marker = new google.maps.Marker({position:latlon,map:map,title:"You are here!"});
+}
+
+function initAutocomplete() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -33.8688, lng: 151.2195 },
+        zoom: 13,
+        mapTypeId: 'roadmap'
+    });
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('direction');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function () {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function (marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+}
+
+function showError(error) {
+    let msg = document.getElementById('map');
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            msg.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            msg.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            msg.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            msg.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
