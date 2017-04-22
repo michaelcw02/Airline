c734cd1440d7c9df8b9d5688d8a5087e84ac8391
Christian <c.fuentes96@hotmail.com>
2017-04-22 10:56:46 -0600

    MVC register

--- client-side/js/Controller/RegisterController.js
+++ client-side/js/Controller/RegisterController.js
@@ -8,47 +8,53 @@ RegisterController.prototype = {
     RegisterController: function(model,view){
         this.model = model;
         this.view = view;
+    },
+    location: function() {
+        return getLocation();
+    },
+    areBlanks: function() {
+        let blanks = false;
+        blanks = isBlank($('#name')[0]);
+        blanks = isBlank($('#lastname')[0]);
+        blanks = isBlank($('#birthdate')[0]);
+        blanks = isBlank($('#email')[0]);
+        blanks = isBlank($('#password')[0]);
+        blanks = isBlank($('#passwordRepeat')[0]);
+        return blanks;
+    },
+    doValidate: function() {
+    // if (areBlanks) 
+    //    window.alert("ESPACIOS EN BLANCO");   //make the pop up!
+    //  if (!samePassword)
+    //     window.alert("CONTRASEÑAS DIFERENTES"); 
+        window.alert("hola");
+    },
+    isBlank: function(element) {
+        element.classList.remove('invalid');
+        if (element.value.length == 0) {
+            element.classList.add('invalid');
+            return true;
+        }
+    },
+
+    isNumber: function(){
+        var regex = /^\(?\d{3}\)?-?\s*-?\d{4}$/;
+        if(regex.test(phno.telephone.value)){
+            return true;
+        }
+        else{
+            alert("This is not a valid phone number");
+            return false;
+        }
+    },
+
+    samePassword: function(){
+        $('#passwordRepeat').keyup(function(){
+            var pass_1 = $('#password').val();
+            var pass_2 = $('#passwordRepeat').val();
+            if(pass_1 != pass_2 && pass_2 != ''){
+                return true;
+            }
+        });
     }
-}
-
-
-function areBlanks() {
-    let blanks = false;
-    blanks = isBlank($('#name')[0]);
-    blanks = isBlank($('#lastname')[0]);
-    blanks = isBlank($('#birthdate')[0]);
-    blanks = isBlank($('#email')[0]);
-    blanks = isBlank($('#password')[0]);
-    blanks = isBlank($('#passwordRepeat')[0]);
-    return blanks;
-}
-function isBlank(element) {
-    element.classList.remove('invalid');
-    if (element.value.length == 0) {
-        element.classList.add('invalid');
-        return true;
-    }
-}
-
-function isNumber(){
-    var regex = /^\(?\d{3}\)?-?\s*-?\d{4}$/;
-    if(regex.test(phno.telephone.value)){
-  
-    return true;
-    }
-    else{
-      alert("This is not a valid phone number");
-      return false;
-  }
-}
-
-
-function samePassword(){
-   	$('#passwordRepeat').keyup(function(){
-		var pass_1 = $('#password').val();
-		var pass_2 = $('#passwordRepeat').val();
-		if(pass_1 != pass_2 && pass_2 != ''){
-			return true;
-		}
-	});
 }
