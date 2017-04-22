e4a82b4d6d1935fd1d340c0b0017495855d8f89c
Christian <c.fuentes96@hotmail.com>
2017-04-22 14:36:00 -0600

    mvc changes

--- client-side/js/View/IndexView.js
+++ client-side/js/View/IndexView.js
@@ -10,9 +10,15 @@ function addElementsProperties() {
     $('#btnRoundTrip').click( () => $("#returning").show() );
     $('#btnOneWay').click( () => $("#returning").hide() );
     $("#departing").datepicker({ minDate: new Date() });
+    //showCities($("#from").first());
+    //showCities($("#to").first());
     $("#returning").datepicker( () => controller.returningDate() );
     $('#btnDecrease').click( () =>  controller.decreaseAdults() );
     $('#btnIncrease').click( () => controller.increaseAdults() );
+}
+
+function showCities(){
+    window.alert("hello");
 }
 
 $(loadPage);
