1b2451bfad5d8914c45f65de6986f7285021a1bd
michaelcw02 <michael.cw02@hotmail.com>
2017-04-23 22:36:17 -0600

    merged

--- client-side/js/View/IndexView.js
+++ client-side/js/View/IndexView.js
@@ -10,9 +10,14 @@ function addElementsProperties() {
     $('#btnRoundTrip').click( () => $("#returning").show() );
     $('#btnOneWay').click( () => $("#returning").hide() );
     $("#departing").datepicker({ minDate: new Date() });
+<<<<<<< HEAD
     //showCities($("#from").first());
     //showCities($("#to").first());
     $("#returning").datepicker( () => controller.returningDate() );
+=======
+    $("#departing").change( () => controller.setMinReturnDate() );
+    $("#returning").datepicker({ minDate: new Date() } );
+>>>>>>> finished datepickers
     $('#btnDecrease').click( () =>  controller.decreaseAdults() );
     $('#btnIncrease').click( () => controller.increaseAdults() );
 }
